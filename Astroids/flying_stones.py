29f1f8f256a7a0f259fb7b9e02ae8e400075799f
Anton Eklöf <anton_eklof@hotmail.com>
2017-11-09 11:52:33 +0100

    När en stor asteroid blir skjuten så skapas 2 medium asteroidr. Sen kan man skjuta ner de mellanstora så de försvinner.

--- Astroids/flying_stones.py
+++ Astroids/flying_stones.py
@@ -9,10 +9,11 @@ class Stones(Polygon):
 #
     """Asteroidsssss"""
 
-    def __init__(self, choice, x=0, y=0):
+    def __init__(self, choice, x=0, y=0, astroidnman = "L"):
 
         #self.big = pygame.image.load("asteroid_test_stone1.png")
         #self.medium = pygame.image.load("asteroid_test_stone2.png")
+        self.astroidnamn = astroidnman
 
         big_asteroid = [Point(10,30), Point(25,25), Point(33,11), Point(35,-7), Point(21,-13), Point(13,-29), Point(-13,-31), Point(-21,-28), Point(-28,-22), Point(-34,-6), Point(-32,14), Point(-29, 21), Point(-15, 9), Point(-23, 24), Point(-10, 30)]
         
@@ -61,6 +62,8 @@ class Stones(Polygon):
 
     def newMedium(self,x,y):
          self.position = Point(x,y)
+         self.astroidnamn = "M"
+
        
 
 
